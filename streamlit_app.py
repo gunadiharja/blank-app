10d80cecf5e9c13f9298975be95b90c85c2ff2c4
Agun <agunadiharja@gmail.com>
2025-11-20 03:03:52 +0000

    test3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
 import streamlit as st
+import pandas as pd
+
 
 st.title("🎈 My new Streamlit app!")
 st.write(
     "This is the beginning!")
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
